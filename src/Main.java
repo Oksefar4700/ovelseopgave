0f2a99a54f39e8acd58205da8f96cc636fb41c5f
Frederik <razor-for-ever@hotmail.com>
2023-10-01 20:55:39 +0200

    Øvelse

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        int antalTalIndtastet = 0;
+
+        System.out.println("Hej med dig!");
+
+        while(true) {
+            System.out.println("Indtast et tal eller 'Q' for at afslutte");
+            String input = scanner.next();
+
+            //hvis brugeren indtaster "q" eller "Q" aflutter vi løkken.
+            if(input.equalsIgnoreCase("Q")) {
+                break;
+            }
+
+            //vi bruger try-catch så vi kan fange, hvis brugeren IKKE indtaster et tal.
+            try {
+                int brugerIndtastetTal = Integer.parseInt(input);
+                total += brugerIndtastetTal;
+                antalTalIndtastet++;
+
+                double gennemsnittet = (double) total/antalTalIndtastet;
+                System.out.println("Gennemsnittet af talene er: " + gennemsnittet);
+            } catch (NumberFormatException e) {
+                System.out.println("Indtast et tal.");
+            }
+        }
+
+        System.out.println("Programmet er afluttet.");
+
     }
 }
